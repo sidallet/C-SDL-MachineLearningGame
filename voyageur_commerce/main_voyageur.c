8b1c2a5caf4cb0aac19eac5f8a1d3fc109f49c49
sidallet <sidallet@ada.local.isima.fr>
2023-06-27 15:06:52 +0200

    restruct en fichier

--- voyageur_commerce/main_voyageur.c
+++ voyageur_commerce/main_voyageur.c
@@ -1,11 +1,9 @@
 #include <assert.h>
 #include <stdbool.h>
 #include "wrapper_sdl.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "matrice.h"
 
-#define N 10
+
 void draw_rec(SDL_Renderer* renderer,int rectWidth, int rectHeight,int rectx,int recty,int Red,int Green,int Blue)
 {  
     SDL_Rect rect;
@@ -18,50 +16,7 @@ void draw_rec(SDL_Renderer* renderer,int rectWidth, int rectHeight,int rectx,int
 
 }
 
-void genereMatriceArbre(int** matrice, int bas, int haut) {
-    if (bas < haut) {
-        int k = (rand() % (haut - bas)) + bas + 1;
-        matrice[bas][bas+1] = 1;
-        matrice[bas+1][bas] = 1;
-        if (k+1 <= haut) {
-            matrice[bas][k+1] = 1;
-            matrice[k+1][bas] = 1;
-        }
-        genereMatriceArbre(matrice, bas+1, k);
-        genereMatriceArbre(matrice, k+1, haut);
-        
-    }
-}
 
-void initMatrice(int ** matrice)
-{
-    int i;
-
-    for (i = 0; i < N; i++) {
-        matrice[i] = (int*)malloc(N * sizeof(int));
-        for (int j = 0; j < N; j++) {
-            matrice[i][j] = 0;
-        }
-    }
-}
-
-void afficheMatrice(int ** matrice)
-{
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d ", matrice[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void libereMatrice(int ** matrice)
-{
-    for (int i = 0; i < N; i++) {
-        free(matrice[i]);
-    }
-    free(matrice);
-}
 
 
 int main()
---
+++ voyageur_commerce/matrice.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+
+void genereMatriceArbre(int** matrice, int bas, int haut);
+
+void initMatrice(int ** matrice);
+
+void afficheMatrice(int ** matrice);
+
+void libereMatrice(int ** matrice);
