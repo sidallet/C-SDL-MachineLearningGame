5a37bf6bc2243c626cc31be50717162e6439b0d5
sidallet <sidallet@ada.local.isima.fr>
2023-06-27 14:51:28 +0200

    crea matrice

--- voyageur_commerce/main_voyageur.c
+++ voyageur_commerce/main_voyageur.c
@@ -1,6 +1,9 @@
 #include <assert.h>
 #include <stdbool.h>
 #include "wrapper_sdl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 void draw_rec(SDL_Renderer* renderer,int rectWidth, int rectHeight,int rectx,int recty,int Red,int Green,int Blue)
@@ -15,8 +18,26 @@ void draw_rec(SDL_Renderer* renderer,int rectWidth, int rectHeight,int rectx,int
 
 }
 
+void genereMatriceArbre(int** matrice, int bas, int haut) {
+    if (bas < haut) {
+        int k = (rand() % (haut - bas)) + bas + 1;
+        matrice[bas][bas+1] = 1;
+        matrice[bas+1][bas] = 1;
+        if (k+1 <= haut) {
+            matrice[bas][k+1] = 1;
+            matrice[k+1][bas] = 1;
+        }
+        genereMatriceArbre(matrice, bas+1, k);
+        genereMatriceArbre(matrice, k+1, haut);
+        
+    }
+}
+
+
 int main()
 {
+    int N = 10; 
+    int** matrice = (int**)malloc(N * sizeof(int*));
     SDL_Window * window = NULL;
     SDL_Renderer * renderer = NULL;
     SDL_Rect rect = {0,0,1000,800};
@@ -26,7 +47,35 @@ int main()
     {
         return EXIT_FAILURE;
     }
+
+    for (int i = 0; i < N; i++) { //init matrice
+        matrice[i] = (int*)malloc(N * sizeof(int));
+        for (int j = 0; j < N; j++) {
+            matrice[i][j] = 0;
+        }
+    }//fin init
+    genereMatriceArbre(matrice, 0, N-1);
+
+    //affichage matrice
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrice[i][j]);
+        }
+        printf("\n");
+    }
+    
+    //lib de la mémoire
+    for (int i = 0; i < N; i++) {
+        free(matrice[i]);
+    }
+    free(matrice);
+
     clean_sdl(&window,&renderer);
+
     return 0;
 }
+
+
+
+
 
