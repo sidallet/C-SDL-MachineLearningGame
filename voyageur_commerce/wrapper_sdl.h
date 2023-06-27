71844ee90b64fce4130ab803f33c2b9580f15da7
Martin Rouault <martin.rouault@etu.uca.fr>
2023-06-27 14:39:34 +0200

    Mise en fichier du wrapper sdl

---
+++ voyageur_commerce/wrapper_sdl.h
@@ -0,0 +1,18 @@
+#ifndef WRAPPER_SDL_INCLUDED
+#define WRAPPER_SDL_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+
+bool init_sdl();
+
+SDL_Window* construct_window(const char* titre, const SDL_Rect* rect, const Uint32 flags);
+
+SDL_Renderer* construct_renderer(SDL_Window* window);
+
+bool start_sdl(SDL_Window** window, SDL_Renderer** renderer, const char* titre, SDL_Rect* rect);
+
+void clean_sdl(SDL_Window** window, SDL_Renderer** renderer);
+
+#endif //WRAPPER_SDL_INCLUDED
