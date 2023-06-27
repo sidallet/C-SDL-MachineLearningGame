6f3aab8080978dcb35ff35a3eb2825bf2721d97f
sidallet <sidallet@ada.local.isima.fr>
2023-06-27 11:21:42 +0200

    preparation des fichiers de base

--- spaceGame.h
+++ spaceGame.h
@@ -1,4 +1,23 @@
+#ifndef SDL_WRAPPER_INCLUDED
+#define SDL_WRAPPER_INCLUDED
+
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>   
+#include <SDL2/SDL_ttf.h>  
+
+bool init_sdl();
+
+SDL_Window* construct_window(const char* titre, const SDL_Rect* rect, const Uint32 flags);
+
+SDL_Renderer* construct_renderer(SDL_Window* window);
+
+bool start_sdl(SDL_Window** window, SDL_Renderer** renderer, const char* titre, SDL_Rect* rect);
+
+void clean_sdl(SDL_Window** window, SDL_Renderer** renderer);
+
+#endif //SDL_WRAPPER_INCLUDED
+
