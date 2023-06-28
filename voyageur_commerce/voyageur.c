c0c46c3962551580916745807ec8896c45271ccb
Martin Rouault <martin.rouault@etu.uca.fr>
2023-06-28 11:26:13 +0200

    Début génération point

--- voyageur_commerce/voyageur.c
+++ voyageur_commerce/voyageur.c
@@ -1,5 +1,6 @@
 #include "voyageur.h"
 #include <math.h>
+#include <stdbool.h>
 
 int distance_eucli(Point point1,Point point2)
 {
@@ -7,3 +8,10 @@ int distance_eucli(Point point1,Point point2)
     dist =sqrt((point1.x-point2.x)*(point1.x-point2.x)+(point1.y-point2.y)*(point1.y-point2.y));
     return dist;
 }
+
+
+void generer_points(Point points[], const size_t nombre_points, const Uint32 w_zone, const Uint32 h_zone) {
+	bool cases[12*nombre_points];
+
+}
+
