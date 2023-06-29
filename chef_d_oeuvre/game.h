39199655652f7459164d99b0dcc0b433ccee9f70
sidallet <sidallet@ada.local.isima.fr>
2023-06-29 14:02:19 +0200

    plusieurs obstacles

--- chef_d_oeuvre/game.h
+++ chef_d_oeuvre/game.h
@@ -2,6 +2,7 @@
 #define GAME_INCLUDED
 
 #include <SDL2/SDL.h>
+#include <math.h>
 #include "TextureHandler.h"
 #include <SDL2/SDL2_gfxPrimitives.h>
 
@@ -11,7 +12,8 @@ typedef struct
 	SDL_Rect voiture;
 	TextureHandler textureHandler;
 	int deplacement_voiture;
-	SDL_FRect  rect_obstacle;
+	SDL_FRect rect_obstacle[10];
+	int nbVoiture;
 } Game;
 
 Game new_game(SDL_Renderer* renderer, SDL_Rect * rect);
@@ -19,19 +21,21 @@ Game new_game(SDL_Renderer* renderer, SDL_Rect * rect);
 void game_handle_event(Game* game, SDL_Event* event, SDL_Rect* rect_fenetre);
 
 
-void deplacer_obstacle(Game* game,SDL_Rect* rect_fenetre,Uint32 deltatime);
+void deplacer_obstacle(Game* game,SDL_Rect* rect_fenetre, Uint32 deltatime, int distance_parcouru , int nbVoiture);
 void game_update(Game* game,SDL_Rect* rect_fenetre,Uint32 deltatime);
 void liberer_game(Game* game);
 
 
 void game_afficher(const Game* game, SDL_Renderer* renderer,SDL_Rect* rect_fenetre);
-void afficher_obstacle(SDL_Renderer* renderer,const SDL_FRect* rect_obstacle);
+void afficher_obstacle(SDL_Renderer* renderer, const SDL_FRect* rect_obstacle, int nombreVoiture);
 void afficherRoute(SDL_Renderer* renderer, SDL_Texture* texture, const SDL_Rect* rect_fenetre, int distance_parcourue);
+void afficher_texte(SDL_Renderer* renderer,int dist,SDL_Rect* rect_fenetre);
 
 //fonction voiture
 void deplaceGauche(SDL_Rect* voiture, SDL_Rect* fenetre, Uint32 delta_time);
 void deplaceDroite(SDL_Rect* voiture, SDL_Rect* fenetre, Uint32 delta_time);
 void afficherVoiture(SDL_Renderer * renderer, const SDL_Rect * voiture, SDL_Texture * textureVoiture, int inclinaison);
+void voitureAleatoire(Game * game, int pos, SDL_Rect * fenetre);
 
 #endif //GAME_INCLUDED
 
