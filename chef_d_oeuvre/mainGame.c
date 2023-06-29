78a6fdafa6e00b407e08be5ef05b31f269c7f81f
marouault <marouault@ada.local.isima.fr>
2023-06-29 09:13:07 +0200

    PremiereFenetre

--- chef_d_oeuvre/mainGame.c
+++ chef_d_oeuvre/mainGame.c
@@ -1,1 +1,53 @@
- 
+#include "wrapper_sdl.h"
+#include <SDL2/SDL2_framerate.h>
+
+void affichage(SDL_Renderer* renderer) {
+
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+}
+
+
+int main (int argc, char* argv[]) {
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	SDL_Rect rect_fenetre = {100, 100, 1000, 800};
+	if (!start_sdl(&window, &renderer, "Carcheeez", &rect_fenetre)) {
+		return EXIT_FAILURE;
+	}
+
+	FPSmanager fpsManager;
+	SDL_initFramerate(&fpsManager);
+	SDL_setFramerate(&fpsManager, 60);
+	Uint32 delta_time = 0;
+
+	bool actif = true;
+
+	while (actif)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				actif = false;
+				break;
+			
+			default:
+				break;
+			}
+		}
+		
+		affichage(renderer);
+
+		delta_time = SDL_framerateDelay(&fpsManager);
+
+	}
+	
+
+	clean_sdl(&window, &renderer);
+	return EXIT_SUCCESS;
+}
