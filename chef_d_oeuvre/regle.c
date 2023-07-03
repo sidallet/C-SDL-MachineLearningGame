4056b344ac201b7fce00cf8b75a20a91d57e723a
sidallet <sidallet@ada.local.isima.fr>
2023-07-03 11:15:01 +0200

    afficher

--- chef_d_oeuvre/regle.c
+++ chef_d_oeuvre/regle.c
@@ -1,2 +1,24 @@
 #include "regle.h"
 
+void afficherObservation(FILE* flux, const Observation observation) {
+    fprintf(flux, "[");
+    for (int i = 0; i < 5; i++) {
+        if(i == 4)
+        {
+            fprintf(flux, "%d",observation.routes[i]);
+        }
+        else {
+            fprintf(flux, "%d ",observation.routes[i]);
+        }
+            
+    }
+    fprintf(flux, "]");
+}
+
+void afficherRegle(FILE* flux, const Regle regle)
+{
+    afficherObservation(flux,regle.observ);
+    fprintf(flux, "-> %d (%d)\n",regle.decis, regle.priorite);
+}
+
+
