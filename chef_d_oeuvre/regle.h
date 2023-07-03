091b723b8c0c378fe6528dcb8238d2bb4f5429a6
sidallet <sidallet@ada.local.isima.fr>
2023-07-03 11:00:18 +0200

    debut regle

---
+++ chef_d_oeuvre/regle.h
@@ -0,0 +1,31 @@
+ #ifndef REGLE_H
+ #define REGLE_H
+
+
+typedef enum {
+        JOKER=-1, VIDE, LOIN, PROCHE // mur ?
+}EtatRoute;
+
+typedef struct 
+{
+    EtatRoute routes[5]; //Gauchegauche, gauche, centre, droite, droitedroite
+}Observation;
+
+
+typedef enum {
+        GAUCHE, RIEN, DROITE
+    } Decision ;
+
+typedef struct {
+    Observation observ;
+    Decision decis;
+    int priorite;
+}Regle;
+
+void afficherRegle(FILE* flux, const Regle regle);
+void afficherObservation(FILE* flux, const Observation observation);
+
+Regle chargerRegle(FILE* flux);
+
+
+#endif //REGLE_H
