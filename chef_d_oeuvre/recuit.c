dcbcc6443380e6fc1ccad29dd76ba598a386e58c
sidallet <sidallet@ada.local.isima.fr>
2023-07-05 09:13:52 +0200

    + de vision

--- chef_d_oeuvre/recuit.c
+++ chef_d_oeuvre/recuit.c
@@ -16,7 +16,7 @@ Regle genererRegleAlea(Regle regle)
 {
 	for (int i = 0; i < 5; i++)
 	{
-		int randRegle = (rand() % 4)-1;
+		int randRegle = (rand() % NB_ETAT)-1;
 		regle.observ.routes[i] = randRegle;
 	}
 
@@ -56,7 +56,7 @@ TabRegle alterTabRegle(TabRegle tabRegle) {
 	if (random_num < 5) { //Modif observation
 		int iObs = rand() % 5;
 		int observ = tabRegle.regles[iRegle].observ.routes[iObs];
-		tabRegle.regles[iRegle].observ.routes[iObs] = rand_dif(observ, -1, 2);
+		tabRegle.regles[iRegle].observ.routes[iObs] = rand_dif(observ, -1, 4);
 	} else if (random_num == 5) { //Modif deplacement
 		tabRegle.regles[iRegle].decis = rand_dif(tabRegle.regles[iRegle].decis, -1, 1);
 	} else { //Modif priorité
@@ -140,28 +140,9 @@ double init_temperature(SDL_Rect * rect_fenetre) {
 	return -score_max;
 }
 
-/*
-void fisherYatesMelange(Chemin* chemin)
-{
-	for (int i = 0; i < chemin->nombre_elems-1; i++)
-	{
-		int j = rand() % (i +1);
-		int temp = chemin->val[i];
-		chemin->val[i] = chemin->val[j];
-		chemin->val[j] = temp;
-	}
-	chemin->val[chemin->nombre_elems-1] = chemin->val[0];
-
-}
-*/
 
 TabRegle recuit(int nombre_iterations,SDL_Rect * rect_fenetre, size_t nb_parties,TabRegle tabRegle) {
-	// if (N <= 1) {
-	// 	return 0;
-	// }
-
-
-	double temperature = init_temperature(rect_fenetre)/4;
+	double temperature = init_temperature(rect_fenetre);
 	printf("température initiale : %f\n", temperature);
 	double pente = temperature/nombre_iterations;
 	// afficheChemin(&chemin);
