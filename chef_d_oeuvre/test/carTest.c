79141a4196a25fdb3950aad79895d623b97e1212
sidallet <sidallet@ada.local.isima.fr>
2023-07-05 10:13:56 +0200

    autre test unit

--- chef_d_oeuvre/test/carTest.c
+++ chef_d_oeuvre/test/carTest.c
@@ -7,7 +7,7 @@ void carTestCase(void) {
 	SDL_Rect rect_fenetre = {100, 100, 1000, 800};
     Game game = {
 		.voiture = {0,rect_fenetre.h-125,65,110},
-		.nbVoiture = 1,
+		.nbVoiture = 2,
 		.vitesse = 100,
 		.ecart_obstacles = 15,
 		.temps_deplacement = 0,
@@ -17,9 +17,15 @@ void carTestCase(void) {
     game.rect_obstacle[0].w = 65;
     game.rect_obstacle[0].x = 0;
     game.rect_obstacle[0].y = rect_fenetre.h-100;
+
+    game.rect_obstacle[1].h = 110;
+    game.rect_obstacle[1].w = 65;
+    game.rect_obstacle[1].x = 80;
+    game.rect_obstacle[1].y = rect_fenetre.h-450;
     
     Observation obs = creerObservation(&game,game.rect_obstacle,game.nbVoiture,&rect_fenetre);
     assert(obs.routes[2] == CONTACT);
+    assert(obs.routes[3] == PROCHE);
     liberer_game(&game);
 }
 
